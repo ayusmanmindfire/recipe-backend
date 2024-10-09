f8392b90d08c213f7035299d5de4123a65d46fea
Ayusman Parida <ayusmanp@mindfiresolutions.com>
2024-10-09 18:43:25 +0530

    feat(api): Add new api for fetching all users

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,5 +5,6 @@ const router = Router();
 const userController=new UserController();
 
 router.post('/register',userController.registerUser); //route for registering new user.
+router.get('/',userController.fetchAllUser); //route for fetching all users
 
 export default router;
