6ea6df3306d70a1497066c0a5dfd87db3585b64d
Ayusman Parida <ayusmanp@mindfiresolutions.com>
2024-10-10 11:04:31 +0530

    feat(api): Api for user authentication

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,8 +1,9 @@
 import UserModel from "../database/models/UserModel.js";
 import { errorResponse, successResponse } from "../utils/response.js";
 import { registerUserSchema } from "../validation/userValidation.js";
+import jwt from "jsonwebtoken"
 
-
+const secretKey=process.env.JWT_SECRET;
 class UserController{
     //method for register a new user 
     registerUser=async(req,res,next)=>{
@@ -24,6 +25,35 @@ class UserController{
         try {
             const userResponse = await UserModel.find();
             successResponse(res,userResponse,"All users fetched",200)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    //method for user authentication
+    loginUser=async(req,res,next)=>{
+        try {
+            const { email, password } = req.body;
+            if(!password||!email){
+                return errorResponse(res,"Provide email and password",400);
+            }
+            const existingUser = await UserModel.findOne({email,password});
+            if (existingUser) {
+                const token = jwt.sign(
+                    {
+                        id: existingUser._id,
+                        email: existingUser.email,
+                        username: existingUser.username
+                    },
+                    secretKey,
+                    {expiresIn: '24h'}
+    
+                )
+                return successResponse(res,token,"Token generated",200)
+            }
+            else{
+                return errorResponse(res,"User not found",404)
+            }
         } catch (error) {
             next(error)
         }
